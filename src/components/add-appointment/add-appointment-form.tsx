55124d7ad730a62090e9b22519fd0eba2262fa6c
lg255 <lukas.gruber@gmx.net>
2021-06-12 13:08:12 +0200

    implemented add appointment

--- src/components/add-appointment/add-appointment-form.tsx
+++ src/components/add-appointment/add-appointment-form.tsx
@@ -1,36 +1,75 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import { firestore } from '../../Firebase';
+import CalendarContext from './../../context/calendar-context';
+
+interface AppointmentFormValues {
+  description: string;
+  type: string;
+  date: string;
+}
 
 const AddAppointmentForm: React.FunctionComponent = () => {
+  const { currentUser } = useContext(CalendarContext);
+
+  const saveAppointment = (values: AppointmentFormValues) => {
+    firestore.collection('Appointments').add({
+      description: values.description,
+      type: 'reminder',
+      date: new Date(values.date),
+      userId: currentUser?.userId,
+    });
+  };
+
+  const submitFn = (
+    values: AppointmentFormValues,
+    { setSubmitting }: { setSubmitting: (val: boolean) => void }
+  ) => {
+    saveAppointment(values);
+    setSubmitting(false);
+  };
+
   return (
     <div>
       <Formik
         initialValues={{
           description: '',
           date: '',
+          type: 'reminder',
         }}
         validationSchema={Yup.object({
           description: Yup.string().required('Bitte ausfüllen.'),
           date: Yup.date().required('Required'),
         })}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
+        onSubmit={submitFn}
       >
         <Form>
-          <label htmlFor="description">Titel</label>
-          <Field name="description" id="description" type="text" />
-          <ErrorMessage name="description" />
-          <br />
-          <label htmlFor="date">Datum</label>
-          <Field name="date" id="date" type="date" />
-          <ErrorMessage name="date" />
-          <br />
-          <button type="submit">Speichern</button>
+          <div className="row">
+            <div className="form-label">
+              <label htmlFor="description">Titel</label>
+            </div>
+            <div className="form-input">
+              <Field name="description" id="description" type="text" />
+            </div>
+            <div className="form-error">
+              <ErrorMessage name="description" className="form-error" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="form-label">
+              <label htmlFor="date">Datum</label>
+            </div>
+            <div className="form-input">
+              <Field name="date" id="date" type="date" />
+            </div>
+            <div className="form-error">
+              <ErrorMessage name="date" />
+            </div>
+          </div>
+          <div className="row">
+            <button type="submit">Speichern</button>
+          </div>
         </Form>
       </Formik>
     </div>
